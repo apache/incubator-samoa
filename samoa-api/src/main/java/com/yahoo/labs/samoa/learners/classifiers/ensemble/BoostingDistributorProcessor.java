0b43bb3c848f8ff73dca72438f44a4260e411f40
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-02-24 18:37:38 +0100

    SAMOA-19: Update info in pom.xml

--- samoa-api/src/main/java/com/yahoo/labs/samoa/learners/classifiers/ensemble/BoostingDistributorProcessor.java
+++ samoa-api/src/main/java/com/yahoo/labs/samoa/learners/classifiers/ensemble/BoostingDistributorProcessor.java
@@ -6,7 +6,7 @@ import com.yahoo.labs.samoa.learners.InstanceContentEvent;
  * #%L
  * SAMOA
  * %%
- * Copyright (C) 2013 Yahoo! Inc.
+ * Copyright (C) 2014 - 2015 Apache Software Foundation
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
