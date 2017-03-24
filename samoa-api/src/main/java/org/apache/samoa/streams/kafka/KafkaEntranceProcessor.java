2e6d502a2909731cb9859ba3403407e6a33a2fc2
pwawrzyniak <piotr@wawrzyniak.org.pl>
2017-03-24 14:34:49 +0100

    Tests for kafkaUtils and KafkaEntranceProcessor, minor changes in classes

--- samoa-api/src/main/java/org/apache/samoa/streams/kafka/KafkaEntranceProcessor.java
+++ samoa-api/src/main/java/org/apache/samoa/streams/kafka/KafkaEntranceProcessor.java
@@ -15,6 +15,27 @@
  */
 package org.apache.samoa.streams.kafka;
 
+/*
+ * #%L
+ * SAMOA
+ * %%
+ * Copyright (C) 2014 - 2017 Apache Software Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -97,4 +118,11 @@ public class KafkaEntranceProcessor implements EntranceProcessor {
         KafkaEntranceProcessor kep = (KafkaEntranceProcessor) processor;
         return new KafkaEntranceProcessor(new KafkaUtils(kep.kafkaUtils), kep.deserializer, kep.topic);
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        kafkaUtils.closeConsumer();
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
