a92b303de33d4e317bca75e8ec80f0869df1df47
fobeligi <faybeligianni@gmail.com>
2017-05-20 17:24:21 +0300

    SAMOA-70: Fix bug in DenseInstance toString method which resulted in always writing an arff file with class equal to zero. fix #60

--- samoa-instances/src/main/java/org/apache/samoa/instances/SparseInstance.java
+++ samoa-instances/src/main/java/org/apache/samoa/instances/SparseInstance.java
@@ -24,6 +24,8 @@ package org.apache.samoa.instances;
  * #L%
  */
 
+import java.text.SimpleDateFormat;
+
 /**
  * 
  * @author abifet
@@ -47,4 +49,38 @@ public class SparseInstance extends SingleLabelInstance {
     super(weight, attributeValues, indexValues, numberAttributes);
   }
 
+  @Override
+  public String toString() {
+    StringBuffer str = new StringBuffer();
+
+    str.append("{");
+
+    for (int i=0; i<this.numAttributes()-1;i++){
+      if (!this.isMissing(i)) {
+
+        //if the attribute is Nominal we print the string value of the attribute.
+        if (this.attribute(i).isNominal()) {
+          int valueIndex = (int) this.value(i);
+          String stringValue = this.attribute(i).value(valueIndex);
+          str.append(i).append(" ").append(stringValue).append(",");
+        } else if (this.attribute(i).isNumeric()) {
+          //if the attribute is numeric we print the value of the attribute only if it is not equal 0
+          if (this.value(i) != 0) {
+            str.append(i).append(" ").append(this.value(i)).append(",");
+          }
+        } else if (this.attribute(i).isDate()) {
+          SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+          str.append(i).append(" ").append(dateFormatter.format(this.value(i))).append(",");
+        }
+      } else { //represent missing values
+        str.append(i).append(" ").append("?,");
+      }
+    }
+    //append the class value at the end of the instance.
+    str.append(classIndex()).append(" ").append(this.classAttribute().value((int)classValue()));
+
+    str.append("}");
+
+    return str.toString();
+  }
 }
