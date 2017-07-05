84c94874f6d80fbf2b232beb2f573cd6b478435c
pwawrzyniak <piotr@wawrzyniak.org.pl>
2017-07-05 09:09:19 +0200

    Correction in order of messages

--- samoa-api/src/main/java/org/apache/samoa/streams/kafka/KafkaEntranceProcessor.java
+++ samoa-api/src/main/java/org/apache/samoa/streams/kafka/KafkaEntranceProcessor.java
@@ -13,25 +13,6 @@
  */
 package org.apache.samoa.streams.kafka;
 
-/*
- * #%L
- * SAMOA
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -101,7 +82,7 @@ public class KafkaEntranceProcessor implements EntranceProcessor {
     @Override
     public ContentEvent nextEvent() {
         // assume this will never be called when buffer is empty!        
-        return this.deserializer.deserialize(buffer.remove(buffer.size() - 1));
+        return this.deserializer.deserialize(buffer.remove(0));
     }
 
     @Override
@@ -118,7 +99,7 @@ public class KafkaEntranceProcessor implements EntranceProcessor {
     @Override
     protected void finalize() throws Throwable {
         kafkaUtils.closeConsumer();
-        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+        super.finalize();
     }
     
 }
