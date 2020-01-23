3111878077ef048d6b00fc1325b4af8462c89806
csterling <coreytsterling@gmail.com>
2020-01-23 14:32:58 +1300

    Updated licencing information.
    - Modified Maven to use checkstyle plugin to check as many file licence headers as possible. Not all are possible but it should provide a helping hand in future.
    - Updated headers in all relevant files.
    - Moved notices about bundled code from NOTICE to LICENSE

--- samoa-api/src/main/java/org/apache/samoa/learners/classifiers/rules/common/TargetMean.java
+++ samoa-api/src/main/java/org/apache/samoa/learners/classifiers/rules/common/TargetMean.java
@@ -1,50 +1,23 @@
 /*
- *    TargetMean.java
- *    Copyright (C) 2014 - 2015 Apache Software Foundation
- *    @author  J. Duarte, A. Bifet, J. Gama
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *    
- *    
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package org.apache.samoa.learners.classifiers.rules.common;
 
-/*
- * #%L
- * SAMOA
- * %%
- * Copyright (C) 2014 - 2015 Apache Software Foundation
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-/**
- * Prediction scheme using TargetMean:
- * TargetMean - Returns the mean of the target variable of the training instances
- * 
- * @author Joao Duarte 
- * 
- *  */
+package org.apache.samoa.learners.classifiers.rules.common;
 
 import org.apache.samoa.instances.Instance;
 import org.apache.samoa.moa.classifiers.AbstractClassifier;
@@ -58,6 +31,13 @@ import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.github.javacliparser.FloatOption;
 
+/**
+ * Prediction scheme using TargetMean:
+ * TargetMean - Returns the mean of the target variable of the training instances
+ *
+ * @author Joao Duarte
+ *
+ *  */
 public class TargetMean extends AbstractClassifier implements Regressor {
 
   /**
